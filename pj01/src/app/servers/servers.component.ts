dc859930b619a8b0b6b9b35afe2d8289ca6e8322
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-17 08:52:00 -0300

    feat: component selector alternatives

--- pj01/src/app/servers/servers.component.ts
+++ pj01/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]', -> <div app-servers></div> (CSS Atribute)
+  // selector: 'app-servers', -> <div class="app-servers"></div> (CSS Class)
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
