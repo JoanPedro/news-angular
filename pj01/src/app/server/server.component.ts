cc8e6b471bc6ca369ab98fa6ef53cef596d2d363
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-18 09:19:37 -0300

    feat: adiciona o ngStyle para estilizacao dinamica

--- pj01/src/app/server/server.component.ts
+++ pj01/src/app/server/server.component.ts
@@ -9,11 +9,19 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus () {
     return this.serverStatus
   }
 
   getServerId () {
     return this.serverId
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
   }
 }
