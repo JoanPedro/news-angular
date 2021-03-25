f8ef7a04d4e3cb2352330d946fd5966091ff3d29
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-25 10:02:12 -0300

    feat: Extra feature: SERVICE LAZILY LOADING (LAZY LOADING)

--- pj08/src/app/shared/logging.service.ts
+++ pj08/src/app/shared/logging.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class LogginService {
   public logStatusChange: (status: String) => void = (status) => {
     console.log('A server status changed, new status: ' + status);
