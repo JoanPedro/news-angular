ff7b1bd8ac3062e1323519944f3cedb5df376a3f
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-28 20:32:40 -0300

    feat: Practicing and some Common Gotchas in passing parameters in router and getting on components

--- pj09/src/app/servers/server/server.component.ts
+++ pj09/src/app/servers/server/server.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -10,10 +11,22 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private readonly activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // const id: number = Number.parseInt(this.activatedRoute.snapshot.params['id'] as string);
+    const id: number = +(this.activatedRoute.snapshot.params['id'] as string);
+    this.server = this.serversService.getServer(id);
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        // const id: number = Number.parseInt(params['id'] as string);
+        const id: number = +(params['id'] as string);
+        this.server = this.serversService.getServer(id);
+      }
+    );
   }
 
 }
