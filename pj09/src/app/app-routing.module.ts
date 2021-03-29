a69e5857cb17217b21c4750aefc8e948731ae7d9
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-29 17:39:48 -0300

    feat: Protecting routes with canActivate on routes! an 'middleware' to parent route

--- pj09/src/app/app-routing.module.ts
+++ pj09/src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
+import { AuthGuard } from './auth-guard.service';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
-import { HomeComponent } from './home/home.component';
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from "@angular/core";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent }
   ]},
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers', canActivate: [ AuthGuard ], component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ]},
