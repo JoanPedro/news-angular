dfc11d76bbdc174c80eeed4e5f617311593dc63c
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-30 20:53:02 -0300

    feat: Retrieving route parameters

--- pj05/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ pj05/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private readonly activatedRouter: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRouter.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+
+          // IF HAS ID -> IN EDIT MODE BECAUSE ROUTE .../1/edit
+          // IF NOT -> ../edit. IT DOESN'T HAVE ID
+          this.editMode = params['id'] != null;
+        }
+      )
   }
 
 }
