d7ade223b5e6b96291d57002536ffef0f7b1252d
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-03-31 18:23:03 -0300

    feat: Template Forms - Submitting and Using the ngSubmit directive angular

--- pj11/src/app/app.component.ts
+++ pj11/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,15 @@ export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  onSubmit(form: NgForm) {
+    const formValue: SimpleForm = form.value;
+    console.log(formValue)
+  }
+}
+
+export type SimpleForm = {
+  email: String,
+  secret: String,
+  username: String
 }
