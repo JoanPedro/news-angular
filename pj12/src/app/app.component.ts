cd84888a8519d961ff132e68b275ee11ad1c66d0
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-02 09:34:57 -0300

    feat: Reactive creating custom validator and using arrow function to bind THIS

--- pj12/src/app/app.component.ts
+++ pj12/src/app/app.component.ts
@@ -10,10 +10,12 @@ export class AppComponent implements OnInit {
   genders: Array<String> = ['male', 'female'];
   signupForm: FormGroup;
 
+  forbiddenUsernames = ['Chris', 'Anna'];
+
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username':  new FormControl(null, [ Validators.required ]),
+        'username':  new FormControl(null, [ Validators.required, this.forbiddenNames ]),
         'email': new FormControl(null, [
             Validators.required,
             Validators.email
@@ -32,5 +34,11 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control: FormControl = new FormControl(null, [ Validators.required ]);
     (this.signupForm.get('hobbies') as FormArray).push(control);
+  }
+
+  forbiddenNames: (control: FormControl) => { [s: string]: boolean } = (control) => {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1) return {'nameIsForbidden': true};
+
+    return null;
   }
 }
