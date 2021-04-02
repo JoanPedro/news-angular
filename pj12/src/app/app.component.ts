01a4d022ea6f00fcb234f5a4ff46e685178c523e
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-02 09:58:55 -0300

    feat: Reactive Creeating a CUstom Async Validator

--- pj12/src/app/app.component.ts
+++ pj12/src/app/app.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  genders: Array<String> = ['male', 'female'];
+  genders: Array<String> = ["male", "female"];
   signupForm: FormGroup;
 
-  forbiddenUsernames = ['Chris', 'Anna'];
+  forbiddenUsernames = ["Chris", "Anna"];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'userData': new FormGroup({
-        'username':  new FormControl(null, [ Validators.required, this.forbiddenNames.bind(this) ]),
-        'email': new FormControl(null, [
-            Validators.required,
-            Validators.email
-          ]
-        )
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          [this.forbiddenEmails]
+        ),
       }),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
-    })
+      gender: new FormControl("male"),
+      hobbies: new FormArray([]),
+    });
   }
 
   onSubmit() {
@@ -32,13 +36,24 @@ export class AppComponent implements OnInit {
   }
 
   onAddHobby() {
-    const control: FormControl = new FormControl(null, [ Validators.required ]);
-    (this.signupForm.get('hobbies') as FormArray).push(control);
+    const control: FormControl = new FormControl(null, [Validators.required]);
+    (this.signupForm.get("hobbies") as FormArray).push(control);
   }
 
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
-    if(this.forbiddenUsernames.indexOf(control.value) !== -1) return {'nameIsForbidden': true};
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1)
+      return { nameIsForbidden: true };
 
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        control.value === "test@test.com"
+          ? resolve({ emailIsForbidden: true })
+          : resolve(null);
+      }, 1500);
+    });
   }
 }
