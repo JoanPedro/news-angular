650436f260226a2ac5cd9e3e1ccfc1ba67a7c9ac
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-02 09:36:00 -0300

    feat: Alternative form to change arrow function to .bind(this) method

--- pj12/src/app/app.component.ts
+++ pj12/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username':  new FormControl(null, [ Validators.required, this.forbiddenNames ]),
+        'username':  new FormControl(null, [ Validators.required, this.forbiddenNames.bind(this) ]),
         'email': new FormControl(null, [
             Validators.required,
             Validators.email
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit {
     (this.signupForm.get('hobbies') as FormArray).push(control);
   }
 
-  forbiddenNames: (control: FormControl) => { [s: string]: boolean } = (control) => {
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if(this.forbiddenUsernames.indexOf(control.value) !== -1) return {'nameIsForbidden': true};
 
     return null;
