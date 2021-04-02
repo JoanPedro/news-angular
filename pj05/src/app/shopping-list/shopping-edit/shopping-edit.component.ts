05e6396c26ba0f2ed62aecba1fe76c1dd81f2cf8
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-02 12:27:23 -0300

    feat: Using Template-Form approach to shoppint add item

--- pj05/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ pj05/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,23 @@
 import { ShoppingListService } from './../shopping-list.service';
 import { Ingredient } from './../../recipes/shared/ingredient.model';
-import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  constructor(private readonly shoppingListService: ShoppingListService) {}
 
-  @ViewChild('nameInput')
-  nameInput: ElementRef;
+  ngOnInit(): void {}
 
-  @ViewChild('amountInput')
-  amountInput: ElementRef;
-
-  constructor(private readonly shoppingListService: ShoppingListService) { }
-
-  ngOnInit(): void {
-  }
-
-  handleAddButton() {
-    const ingredientName: String = (this.nameInput.nativeElement as HTMLInputElement).value;
-    const ingredientAmount: Number = Number.parseInt((this.amountInput.nativeElement as HTMLInputElement).value);
-    this.shoppingListService.addIngredients(new Ingredient (ingredientName, ingredientAmount));
+  handleAddButton(form: NgForm) {
+    console.log(form);
+    const value = form.value;
+    this.shoppingListService.addIngredients(
+      new Ingredient(value.name, value.amount)
+    );
   }
 }
