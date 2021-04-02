ce87cc05feb1be00ee862aa75f79ef892b5d84e0
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-02 11:40:33 -0300

    feat: Adding a custom async validator on a static helper class

--- pj13/src/app/app.component.ts
+++ pj13/src/app/app.component.ts
@@ -10,12 +10,16 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 export class AppComponent implements OnInit {
   projectForm: FormGroup;
 
+  // When passing as reference method that uses "this", ensure that its an arrow function or bind
+  // this! aka. (.bind(this)) on method reference calls.
+
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [
-        Validators.required,
-        CustomValidators.invalidProjectName,
-      ]),
+      projectName: new FormControl(
+        null,
+        [Validators.required, CustomValidators.invalidProjectName],
+        [CustomValidators.asyncInvalidProjectName]
+      ),
       email: new FormControl(null, [Validators.required, Validators.email]),
       projectStatus: new FormControl("critical", [Validators.required]),
     });
--- pj13/src/app/custom-validators.ts
+++ pj13/src/app/custom-validators.ts
@@ -1,9 +1,22 @@
 import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
 
 export class CustomValidators {
   static invalidProjectName(control: FormControl): { [key: string]: boolean } {
     if (control.value === "Test") return { invalidProjectName: true };
 
     return null;
+  }
+
+  static asyncInvalidProjectName(
+    control: FormControl
+  ): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        control.value === "Testproject"
+          ? resolve({ invalidProjectName: true })
+          : resolve(null);
+      }, 2000);
+    });
   }
 }
