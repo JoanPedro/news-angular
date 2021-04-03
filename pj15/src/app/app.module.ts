7f443e96a2818575762a86d5435823e6327d8ca6
joan.o.souza <joan.o.souza@ericssoninovacao.com.br>
2021-04-03 12:04:00 -0300

    feat: Creating a custom pipe for shorten names

--- pj15/src/app/app.module.ts
+++ pj15/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShortenPipe } from "./shorten.pipe";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
@@ -5,7 +6,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShortenPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
