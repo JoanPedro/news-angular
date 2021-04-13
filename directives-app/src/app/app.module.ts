a62b199a642a564351c3782508b13e1cf35f0d90
Joan Pedro <jpos.senai@gmail.com>
2021-04-13 12:01:37 -0300

    feat: Adds switch directive

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { SubNgIfComponent } from './ngif/sub-ng-if/sub-ng-if.component';
 import { NgforComponent } from './ngfor/ngfor.component';
 import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { NgTemplateComponent } from './ng-template/ng-template.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { NgSwitchComponent } from './ng-switch/ng-switch.component';
     SubNgIfComponent,
     NgforComponent,
     NgForFormComponent,
-    NgSwitchComponent
+    NgSwitchComponent,
+    NgTemplateComponent
   ],
   imports: [
     BrowserModule,
