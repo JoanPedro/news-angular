203ef2613acf08d1a01da0fea059a728c9568978
Joan Pedro <jpos.senai@gmail.com>
2021-04-15 16:23:29 -0300

    feat: settup

---
+++ products-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { DepartmentFormComponent } from './department-form/department-form.component';
+import { ProductsTableComponent } from './products-table/products-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductFormComponent,
+    DepartmentFormComponent,
+    ProductsTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
