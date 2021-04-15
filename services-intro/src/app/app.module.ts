ab352c75e3e69313443c68d954cdd3172af886dc
Joan Pedro <jpos.senai@gmail.com>
2021-04-15 12:43:54 -0300

    feat: add material & routing module

--- services-intro/src/app/app.module.ts
+++ services-intro/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { AppRoutingModule } from './app.routing.module';
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
