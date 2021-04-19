38b86f707d039c5d0d81b0868105d430c2dd6cda
Joan Pedro <jpos.senai@gmail.com>
2021-04-19 12:54:22 -0300

    feat: adds basic-reation

--- rxjs/src/app/app.module.ts
+++ rxjs/src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BasicCreationComponent } from './basic-creation/basic-creation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicCreationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rxjs/src/app/basic-creation/basic-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-creation',
+  templateUrl: './basic-creation.component.html',
+  styleUrls: ['./basic-creation.component.css']
+})
+export class BasicCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
