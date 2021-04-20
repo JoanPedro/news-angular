4dab1acaaa06bd0cfcb78dceeb80fe92920b03f4
Joan Pedro <jpos.senai@gmail.com>
2021-04-20 20:12:04 -0300

    feat: finish detail product component

--- app-training/src/app/products/product-detail/product-detail.component.ts
+++ app-training/src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,26 @@
+import { Product } from './../../shared/models/product.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = "Product Detail";
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.pageTitle += ` ${id}`;
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
