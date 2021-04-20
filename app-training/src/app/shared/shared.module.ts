148f3b464db833b4adb4d3de71454322141a1019
Joan Pedro <jpos.senai@gmail.com>
2021-04-20 20:10:54 -0300

    feat: adds pipes & star component

--- app-training/src/app/shared/shared.module.ts
+++ app-training/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StarComponent } from './star/star.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StarComponent
+  ],
   imports: [
     CommonModule
   ]
